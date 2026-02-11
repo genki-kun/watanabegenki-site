be60e4518e3d1a60928fda18163ecc585f0ba966
genki-kun <genkikun0513@gmail.com>
2026-02-11 19:38:29 +0900

    feat: setup OGP for watanabegenki.com

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,30 @@ import "./globals.css";
 import Navigation from "@/components/Navigation";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://watanabegenki.com"),
   title: "Genki Watanabe",
   description: "Personal website of Genki Watanabe",
+  openGraph: {
+    title: "Genki Watanabe",
+    description: "Personal website of Genki Watanabe",
+    url: "https://watanabegenki.com",
+    siteName: "Genki Watanabe",
+    images: [
+      {
+        url: "/ogp.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Genki Watanabe",
+    description: "Personal website of Genki Watanabe",
+    images: ["/ogp.png"],
+  },
 };
 
 export default function RootLayout({
